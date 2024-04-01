00db501c781028fd0490da564c03a89ee7baa6fd
asdfghjksdf <124699219+asdfghjksdf@users.noreply.github.com>
2024-04-01 13:59:27 +0530

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching jaces.xyz","Watching jaces.xyz"];
+const statusMessages = ["Watching Ps99 MM,Watching Ps99 MM"];
 
 
 let currentIndex = 0;
