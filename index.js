689ad84c2e778800109d2431de909e192be0f95f
asdfghjksdf <124699219+asdfghjksdf@users.noreply.github.com>
2024-04-14 12:12:06 +0530

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Listening to Shinegami's bot cooking","Shinontop"];
 
 
 let currentIndex = 0;
